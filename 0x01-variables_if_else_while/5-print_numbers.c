ac342f21da589afeda999ec5649e543424203309
AbeerEs <abeeressam85@yahoo.com>
2023-08-11 15:35:30 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: writing numbers from 0 to 9
+ *
+ * Return: Always 0 (success)
+*/
+int main(void)
+{
+int digit = 0;
+while (digit <= 9)
+{
+printf("%i", digit);
+digit++;
+}
+printf("\n");
+return (0);
+}
