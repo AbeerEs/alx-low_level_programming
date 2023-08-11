de0a02b3c8c73b72d5092fc9254f87b8ffb269b0
AbeerEs <abeeressam85@yahoo.com>
2023-08-11 16:01:04 -0700

    k

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: writing numbers from 0 to 9
+ *
+ * Return: Always 0 (success)
+*/
+int main(void)
+{
+int digit = 0;
+while (digit <= 9)
+{
+putchar(digit + 48);
+if (digit != 9)
+{
+putchar(',');
+putchar(' ');
+}
+digit++;
+}
+putchar('\n');
+return (0);
+}
