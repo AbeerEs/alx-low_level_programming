50b5a6980e1a91d8dc75d05c5ac190bd79e8a85b
AbeerEs <abeeressam85@yahoo.com>
2023-08-15 12:53:03 -0700

    L

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* print_alphabet_x10 - entry point
+* description: print alphabet by  using putchar
+*/
+void print_alphabet_x10(void)
+{
+int line;
+int ch;
+for (line = 0; line > 10; line++)
+{
+for (ch = 'a'; ch <= 'z'; ch++)
+_putchar(ch);
+_putchar ('\n');
+}
+}
