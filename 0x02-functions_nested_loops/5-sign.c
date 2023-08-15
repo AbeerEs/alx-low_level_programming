249e8f8047466fc124848b36857406d3ac1598b9
AbeerEs <abeeressam85@yahoo.com>
2023-08-15 14:39:33 -0700

    L

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+* print_sign - entry point
+* @n: number from input of function
+* description: print alphabet by  using putchar
+* Return: (1) for sign + , (0) equal 0 , (-1) for sign -
+*
+*/
+int print_sign(int n)
+{
+        if (n > 0)
+	{
+		_putchar(43);
+                return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(48);
+                return (0);
+	}
+	else
+	{
+		_putchar(45);
+		return (-1);
+	}
+}
