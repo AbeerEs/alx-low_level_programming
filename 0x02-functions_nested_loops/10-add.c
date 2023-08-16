fd2b746c270c2e544b16af258f9f108e93bfb2a4
AbeerEs <abeeressam85@yahoo.com>
2023-08-16 01:12:24 -0700

    L

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *add - add tow numbers
+ *@x: first number
+ *@y: second number
+ *Return: sum of x & y
+ */
+int add(int x, int y)
+{
+	int res = x + y;
+
+	return (res);
+
+}
