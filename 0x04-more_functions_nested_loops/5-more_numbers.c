e6d10cfe1ee04f4f7919323ab8114029d0e8f735
AbeerEs <abeeressam85@yahoo.com>
2023-08-17 03:12:22 -0700

    L

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ *more_numbers - print numbers ten times
+ *
+ *Return: Always 0 (success)
+ */
+void more_numbers(void)
+{
+	int i, j, count;
+
+	for (i = 0; i <= 9 ; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			count = j;
+			if (count > 9)
+			{
+				_putchar(1 + 48);
+				j = count % 10;
+			}
+			_putchar(j + 48);
+		}
+		_putchar('\n');
+	}
+}
