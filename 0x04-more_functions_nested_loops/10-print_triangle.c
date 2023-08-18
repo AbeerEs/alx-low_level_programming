ccaf7023f1a7d4da62237b2e9f92cdb4ffb4e152
AbeerEs <abeeressam85@yahoo.com>
2023-08-18 05:51:14 -0700

    L

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -7,13 +7,20 @@
 void print_triangle(int size)
 {
 	int l, w;
-	
+	int s;
+
 	if (size == 0)
 		_putchar('\n');
 	for (l = 1; l <= size; l++)
 	{
+
+		for (s = size - l; s >= 0; s--)
+		{
+			_putchar(' ');
+		}
 		for (w = 1; w <= l; w++)
 		{
+
 			_putchar('#');
 		}
 		_putchar('\n');
