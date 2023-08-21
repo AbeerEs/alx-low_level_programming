1ebf71d20102bf3b7368c64126522f3cb86b133b
AbeerEs <abeeressam85@yahoo.com>
2023-08-21 13:54:02 -0700

    L

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *rev_string - reverses a string.
+ *@s: input parameter
+ *
+ */
+void rev_string(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+		i++;
+	while (i--)
+		_putchar(s[i]);
+	_putchar('\n');
+}
