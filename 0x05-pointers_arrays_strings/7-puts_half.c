28923d83ba426e0502ab21f4ec5d7bdcf205f142
AbeerEs <abeeressam85@yahoo.com>
2023-08-22 00:42:45 -0700

    L

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *puts_half - print half of input
+ *@str: input parameter
+ *
+ */
+void puts_half(char *str)
+{
+	int i;
+	for (i = 0; str[i] != '\0'; i++)
+		;
+	i++;
+	for (i = 0; str[i] != '\0'; i++)
+		_putchar(str[i]);
+	_putchar('\n');
+}
