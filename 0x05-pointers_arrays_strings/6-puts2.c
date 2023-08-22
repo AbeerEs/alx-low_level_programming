d8395b72ddc759a49db229cdc380493619fbeace
AbeerEs <abeeressam85@yahoo.com>
2023-08-22 00:08:44 -0700

    L

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,11 +8,15 @@
 void puts2(char *str)
 {
 	int i;
+	int counter = 0;
+
+	while (str[counter] != 0)
+		counter++;
 
 	for (i = 0; str[i] != '\n'; i++)
 	{
-		if (i % 2 == 0)
-			_putchar(str[i]);
+		if (counter 2 == 0)
+			_putchar(str[counter]);
 	}
 	_putchar('\n');
 }
