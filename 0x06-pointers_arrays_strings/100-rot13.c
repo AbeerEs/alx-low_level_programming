4d629dc8abe29f3515593429c567c09b0a8dbc33
AbeerEs <abeeressam85@yahoo.com>
2023-08-24 00:56:21 -0700

    L

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ **rot13 - function that encodes a string using rot13.
+ *@s: input parameter
+ *Return: pointer to result
+ *
+ */
+char *rot13(char *s)
+{
+	char s1[] = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz";
+	char s2[] = "NnOoPpQqRrSsTtUuVvWwXxYyZzAaBbCcDdEeFfGgHhIiJjKkLlMm";
+	int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (s[i] == s1[j])
+			s[i] = s2[j];
+			break;
+		}
+	}
+	return (s);
+}
