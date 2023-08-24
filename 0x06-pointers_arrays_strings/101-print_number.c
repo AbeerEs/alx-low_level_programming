f8928bbc1fb6b9b80b31c13dc376d11238e8c4ba
AbeerEs <abeeressam85@yahoo.com>
2023-08-24 02:13:27 -0700

    L

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *print_number - prints an integer
+ *@n: input parameter
+ *Return: void
+ *
+ *
+ */
+void print_number(int n)
+{
+	char s[10];
+	int i = 0;
+	while (s[i] != '\0')
+	{
+		_putchar(s[i]);
+		i++;
+	}
+}
