8e8e4f5784d9e7ca003f36028035167a95b49d08
AbeerEs <abeeressam85@yahoo.com>
2023-08-28 23:20:12 -0700

    L

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -8,13 +8,18 @@
 char *_strstr(char *haystack, char *needle)
 {
 	int i, j;
+	char *p;
 
 	for (i = 0; haystack[i] != '\0'; i++)
 	{
-		for (j = 0; needle[j] != 0; j++)
+		for (j = 0; needle[j] != '\0'; j++)
 		{
-			if (haystack[i] == needle[j])
-				return (needle);
+			if (haystack[i] == needle[j] && needle[j] != '\0')
+			{
+				p = &haystack[i];
+				return (p);
+		
+			}
 		}
 	}
 	return ('\0');
