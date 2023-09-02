d0abf719fb88c1d1bfdf3cf8ad65604a7cdaae39
AbeerEs <abeeressam85@yahoo.com>
2023-09-02 04:51:53 -0700

    L

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *@argc: input parameter
+ *@argv: input parameter
+ *Return: int
+ */
+int main(int argc, char const *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
