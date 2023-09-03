89b8210a850a01c861391761d8ba8fc3e5e3e1ac
AbeerEs <abeeressam85@yahoo.com>
2023-09-03 10:02:32 -0700

    L

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -3,11 +3,11 @@
  *main - entry point
  *@argc: input parameter
  *@argv: input parameter
- *Return: int
+ *Return: alwayes 0
  */
 int main(int argc, char *argv[])
 {
 	(void)argc;
-	printf("%d\n", argc)
+	printf("%d\n", argc - 1)
 		return (0);
 }
