0e3b3d497afa0a655347041c5e76056a2aa2e3a0
AbeerEs <abeeressam85@yahoo.com>
2023-09-04 08:27:58 -0700

    L

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,7 +11,7 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 	char *n = malloc(size);
 
-	if (n == NULL)
+	if (n == 0 || size == 0)
 		return (0);
 
 	for (i = 0; i < size; i++)
