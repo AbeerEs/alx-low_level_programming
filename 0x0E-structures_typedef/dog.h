8b6e29ab583f3dda7bd32ee4b1576c51dbdc7cf2
AbeerEs <abeeressam85@yahoo.com>
2023-09-12 03:37:10 -0700

    L

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ *struct dog - struct dog
+ *@name: input parameter
+ *@age: input parameter
+ *@owner: input parameter
+ *Description: new type struct dog with the following elements
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
