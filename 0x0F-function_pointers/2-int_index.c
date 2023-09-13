137544d061daef8b6365f4edb639a62e71e9484a
AbeerEs <abeeressam85@yahoo.com>
2023-09-13 07:27:55 -0700

    L

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+/**
+ *int_index - Write a function that searches for an integer
+ *@array: input parameter
+ *@size: input parameter
+ *@cmp: input parameter
+ *Return: index parameter
+ *
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+
+	if (array && size && cmp)
+		while (i < size)
+		{
+			if (cmp(array [i]))
+				return (i);
+			i++;
+		}
+	return (-1);
+}
